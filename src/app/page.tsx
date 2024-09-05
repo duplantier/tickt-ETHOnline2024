7f4fca12c2f78f7f360592b629401e7c321a3720
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-09-05 12:41:11 +0300

    [Build]: Web3Auth basic implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
-import { useAccount } from "wagmi";
+import LogInWithWeb3Auth from "@/components/LogInWithWeb3Auth";
 
 export default function Home() {
-  const { address: userCurrentAccountAddress } = useAccount();
   return (
     <div className="grid text-white grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <w3m-button />
-      {userCurrentAccountAddress}
+      <LogInWithWeb3Auth />
     </div>
   );
 }
