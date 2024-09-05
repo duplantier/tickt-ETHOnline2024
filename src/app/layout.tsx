1bbb5be3309ccefadd243e108257c4e5a95c267e
Hüseyin Karataş <huseyinlora.karatas@gmail.com>
2024-09-05 11:41:29 +0300

    [Build]: WalletConnect installation & configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { headers } from "next/headers";
 
+import { cookieToInitialState } from "wagmi";
+
+import { config } from "@/config";
+import AppKitProvider from "@/context";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -23,12 +28,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const initialState = cookieToInitialState(config, headers().get("cookie"));
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppKitProvider initialState={initialState}>{children}</AppKitProvider>
       </body>
     </html>
   );
